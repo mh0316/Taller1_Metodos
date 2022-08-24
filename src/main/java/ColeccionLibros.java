4aeb8b18f8612c84daf86c9a50e991508f2e6de0
Marcelo <m.henriquez11@ufromail.cl>
2022-08-24 09:03:35 -0400

    Primer commit

---
+++ src/main/java/ColeccionLibros.java
@@ -0,0 +1,15 @@
+public class ColeccionLibros {
+    public static void main(String[] args) {
+        menu();
+    }
+
+    public static void menu(){
+        System.out.println("1. Agregar libro");
+        System.out.println("2. Buscar libro");
+        System.out.println("3. Mostrar espacios usados");
+        System.out.println("4. Mostrar espacios disponibles");
+        System.out.println("5. MOstrar toda la colección");
+    }
+
+
+}
