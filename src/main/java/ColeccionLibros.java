c14e798a494876dc4b03e2df09e0ace41c7b1583
Marcelo <m.henriquez11@ufromail.cl>
2022-08-24 09:31:11 -0400

    Segundo commit

--- src/main/java/ColeccionLibros.java
+++ src/main/java/ColeccionLibros.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class ColeccionLibros {
     public static void main(String[] args) {
         menu();
+        agregarLibro();
     }
 
     public static void menu(){
@@ -8,8 +12,53 @@ public class ColeccionLibros {
         System.out.println("2. Buscar libro");
         System.out.println("3. Mostrar espacios usados");
         System.out.println("4. Mostrar espacios disponibles");
-        System.out.println("5. MOstrar toda la colección");
+        System.out.println("5. Mostrar toda la colección");
     }
 
+    public static int ingresar() {
+        Scanner teclado = new Scanner(System.in);
+        int entrada;
+        do {
+            try {
+                entrada = teclado.nextInt();
+                break;
+            } catch (Exception e) {
+                teclado.nextLine();
+            }
+        } while (true);
+        return entrada;
+    }
+
+
+    private void seleccionarOpcionMenu(int opcion){
+        switch (opcion){
+            case 1:
+                System.out.println(agregarLibro());
+            case 2:
+                System.out.println(buscarLibro());
+            case 3:
+                System.out.println(mostrarEspaciosUsados());
+            case 4:
+                System.out.println(mostrarEspaciosDisponibles());
+            case 5:
+                System.out.println(mostrarTodaLaColeccion);
+        }
+    }
+
+    public static String[][] agregarLibro(String misLibros[], String titulo, String autor, String editorial){
+
+    }
+
+    private static boolean buscarLibro(){
+
+        return false;
+    }
+
+    private static void mostrarTotalLibros(char[][] librosContenidos, char[][] espaciosDisponibles) {
+        for (int i = 0; i < librosContenidos.length; i++) {
+            System.out.println(Arrays.toString(librosContenidos[i]));
+            System.out.println(Arrays.toString(espaciosDisponibles[i]));
+        }
+    }
 
 }
