29e751ed6db4ca2aae4885c4819be69e14b73741
Marcelo <m.henriquez11@ufromail.cl>
2022-08-24 10:41:00 -0400

    Quinto commit

--- src/main/java/ColeccionLibros.java
+++ src/main/java/ColeccionLibros.java
@@ -12,7 +12,7 @@ public class ColeccionLibros {
         System.out.println("2. Buscar libro");
         System.out.println("3. Mostrar espacios usados");
         System.out.println("4. Mostrar espacios disponibles");
-        System.out.println("5. Mostrar toda la colección"+mostrarTotalLibros());
+        System.out.println("5. Mostrar toda la colección");
         int opcion = validarIngreso();
         seleccionarOpcionMenu(opcion);
     }
